b2bbac0862ef38566ff000abf27aeb555b439468
Neitrem <a.v.gorshenin000@gmail.com>
2023-05-08 20:51:36 +0300

    create main-page, partly ready html and scc for main-page, now workin on carousel

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
 <template>
-	<MyHeader></MyHeader>
-	<router-view></router-view>
-	<MyFooter></MyFooter>
+	<vHeader></vHeader>
+	
+	<MainPage></MainPage>
+
+	<vFooter></vFooter>
 </template>
 
 <script>
 
-import MyHeader from './components/MyHeader.vue';
-import MyFooter from './components/MyFooter.vue';
+import vFooter from './components/v-footer.vue';
+import vHeader from './components/v-header.vue';
+
+import MainPage from './components/pages/MainPage.vue'
 
 
 export default {
 	name: 'App',
-	components: {MyHeader, MyFooter}
+	components: {vFooter, vHeader, MainPage}
 }
 
 </script>
 
 <style lang="scss">
 @import '@/styles/reset.css';
+
+#app
+{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	min-width: 320px;
+}
+
 
 </style>
